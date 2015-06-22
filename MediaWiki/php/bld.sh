d8a15a472734a991faa632052d534c6411de3887
YoongHM <yoonghm@gmail.com>
2015-06-22 15:15:22 +0800

    Initial version for php/bld.sh

---
+++ MediaWiki/php/bld.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+../configure --prefix=/home/s --mandir=/home/s/share/man --with-openssl      \
+--with-pdo-pgsql=/home/s --with-pgsql=/home/s --enable-opcache --enable-intl \
+--enable-fpm --enable-sockets --with-curl --localstatedir=/home/s/logs       \
+--sbindir=/home/s/bin --datadir=/home/s/logs --sysconfdir=/home/s/conf       \
+--with-fpm-user=www-data --with-fpm-group=www-data --enable-pcntl
