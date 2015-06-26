c6603ef5c1c03184582fcfa4d1d377fd8f4dbefb
YoongHM <yoonghm@gmail.com>
2015-06-26 10:33:59 +0800

    Create LocalSettings.php

---
+++ MediaWiki/LocalSettings.php
@@ -0,0 +1,13 @@
+<?php
+
+# ...
+
+$wgSitename = "EEWiki";           # Change it for other mediawiki site.
+
+$wgScriptPath  = "/w";            # Change it for other mediawiki path
+$wgArticlePath = "/wiki/$1";      # Base URL. Change it for other mediawiki site.
+$wgUsePathInfo = true;            # Use short URL
+
+$wgServer = "http://eewiki";      # Change it for other domain
+
+
